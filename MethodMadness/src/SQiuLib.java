a424367487d8ac389cd4f842b2a43d7ab9b73839
sqiu3255 <32362370+sqiu3255@users.noreply.github.com>
2017-10-10 09:13:47 -0400

    Update SQiuLib.java

--- MethodMadness/src/SQiuLib.java
+++ MethodMadness/src/SQiuLib.java
@@ -1,4 +1,8 @@
 
 public class SQiuLib {
-
+	public String dateStr(String mmddyyyy) {
+		  String a = mmddyyyy.substring(2,4);  // returns dd
+		  String b = mmddyyyy.substring(0,2);  //returns mm
+		  String c = mmddyyyy.substring(4,8); //returns yyyy
+	return a + "-" + b + "-" + c;
 }
