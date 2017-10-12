36181510e651c1f254255e91ea74979a3bcf2dc5
jwu9201 <32456209+jwu9201@users.noreply.github.com>
2017-10-12 19:28:22 -0400

    Update JWuLib.java

--- MethodMadness/src/JWuLib.java
+++ MethodMadness/src/JWuLib.java
@@ -1,18 +1,16 @@
-
 public class JWuLib {
-  public static void isPalindrome (string s) {
-    
+  public static boolean isPalindrome (String s) {
+	  return s.equals(new StringBuilder(s).reverse().toString());
   }
-	public static boolean test (int n) {
-		double m = Math.sqrt(n);
-		double o = (m*m);
-		return (o == n);
+	public static boolean perfectSquare (int n) {
+		int m = (int) Math.sqrt(n);
+		return (m*m == n);
 	}
 	public static boolean isFibonnaci (int y) {
-		return (test(5*y*y+4)) || (test(5*y*y-4));
+		return (perfectSquare((5*y*y) + 4)) || (perfectSquare((5*y*y) - 4));
   }
-  public static void sumUpTo (int x) {
+  public static int sumUpTo (int x) {
     int y = (x*(x+1)/2);
-    System.out.println(y);
+    return (y);
   }
 }
