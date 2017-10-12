a68b3547efdc66c68a55d0fbf9e215bced88ed51
sqiu3255 <32362370+sqiu3255@users.noreply.github.com>
2017-10-12 09:16:34 -0400

    Update SQiuLib.java

--- MethodMadness/src/SQiuLib.java
+++ MethodMadness/src/SQiuLib.java
@@ -1,15 +1,40 @@
 
-public class SQiuLib {
-	public String dateStr(String mmddyyyy) {
-		  String a = mmddyyyy.substring(2,4);  // returns dd
-		  String b = mmddyyyy.substring(0,2);  //returns mm
-		  String c = mmddyyyy.substring(4,8); //returns yyyy
-	return a + "-" + b + "-" + c;
-}
+public class SQiuLib{
 	
-	public String cutOut(String mainStr, String subStr) {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//String Method: dateStr
+		
+		String dateStr = new String ("mm/dd/yyyy"); {
+			  String a = dateStr.substring(3,5);  // returns dd
+			  String b = dateStr.substring(0,2);  //returns mm
+			  String c = dateStr.substring(6,10); //returns yyyy
+		System.out.println(a + "-" + b + "-" + c);
+	}
+		
+	//String Method: cutOut	
+		
+	String cutOUt = new String (mainStr, subStr) {
 		  String a = subStr.substring(0,1);  // first occurrence of subStr cutout
-		  
+	
 		  return mainStr + a;
 	}
+	
+	//Math Method: multiplicationTable
+	
+	{
+		int base;
+		int range; 
+		
+		for (int n = 0 ; n <= 10 ; n++); 
+		
+	}
+	
+	
+		System.out.println(base * n);
+	
+	
+	
+}
 }
