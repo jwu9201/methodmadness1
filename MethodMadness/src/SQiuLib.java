6c8c18dc25b34327d1871a5df4d8ece1b79843b7
jwu9201 <32456209+jwu9201@users.noreply.github.com>
2017-10-12 20:44:22 -0400

    Update SQiuLib.java

--- MethodMadness/src/SQiuLib.java
+++ MethodMadness/src/SQiuLib.java
@@ -1,40 +1,28 @@
 
 public class SQiuLib{
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
 	//String Method: dateStr
-		
-		String dateStr = new String ("mm/dd/yyyy"); {
-			  String a = dateStr.substring(3,5);  // returns dd
+		public static String dateStr (String dateStr) {
+			  String a = dateStr.substring(3,5);  //returns dd
 			  String b = dateStr.substring(0,2);  //returns mm
 			  String c = dateStr.substring(6,10); //returns yyyy
-		System.out.println(a + "-" + b + "-" + c);
-	}
+			  return (a + "-" + b + "-" + c);
+		}
+		//String Method: cutOut	
 		
-	//String Method: cutOut	
+		public static String cutOut (String mainStr , String subStr) {
+			  String a = subStr.substring(0,1);  // first occurrence of subStr cutout
 		
-	String cutOUt = new String (mainStr, subStr) {
-		  String a = subStr.substring(0,1);  // first occurrence of subStr cutout
-	
-		  return mainStr + a;
-	}
-	
-	//Math Method: multiplicationTable
-	
-	{
-		int base;
-		int range; 
+			  return mainStr + a;
+		}
 		
-		for (int n = 0 ; n <= 10 ; n++); 
+		//Math Method: multiplicationTable
 		
-	}
-	
-	
-		System.out.println(base * n);
-	
-	
-	
+		public static void multiplicationTable(int base, int range) {
+			
+			for (int n = 0 ; n <= range ; n++)
+				System.out.print(base * n + " ");
+		}
+		
 }
-}
+	
