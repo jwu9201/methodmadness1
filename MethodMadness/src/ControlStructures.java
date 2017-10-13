1b9084d2f153a7daf7519ff0fceb1449b0369652
jwu9201 <32456209+jwu9201@users.noreply.github.com>
2017-10-12 21:02:47 -0400

    Update ControlStructures.java

--- MethodMadness/src/ControlStructures.java
+++ MethodMadness/src/ControlStructures.java
@@ -2,8 +2,12 @@
 public class ControlStructures {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub 
+		System.out.println(JWuLib.isPalindrome("abc")); 	//Input = String, Output = True/false
+		System.out.println(JWuLib.isFibonnaci(8)); 		//Input = integer, Output = True/false
+		System.out.println(JWuLib.sumUpTo(10)); 		//Input = integer, Output = Sum of all integers up to Input
+		System.out.println(SQiuLib.dateStr("mm/dd/yyyy")); 	//Input = string in format "mm/dd/yyyy", Output = string in format "mm-dd-yyyy"
+		System.out.println(SQiuLib.cutOut("newyork", "york"));  //Input = mainStr and subStr, Output = mainStr with the first occurence of subStr cutout
+		SQiuLib.multiplicationTable(10, 5);			//Input = base and range, Output = A multiplication table displaying the base*n for n= all integers from 0 to range
 	}
-
 }
